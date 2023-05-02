702211f9cbc9ec1b57bdadf38d41a5158303fc02
Julien RICHARD <julien.y.richard@gmail.com>
2023-05-02 11:16:58 +0200

    Récupération de la data de filtre

--- src/components/StatusFilter.jsx
+++ src/components/StatusFilter.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import { useEffect } from 'react'
 
 const StatusFilter = () => {
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_BACKEND_URL}/status`)
+      .then(res => res.json())
+      .then(status => console.log(status))
+      .catch(err => console.error(err))
+  }, [])
+
   return (
     <div>StatusFilter</div>
   )
