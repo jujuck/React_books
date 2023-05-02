ff5ac9a71a800117b111757855b86e71ec1d4772
Julien RICHARD <julien.y.richard@gmail.com>
2023-05-02 10:52:20 +0200

    Mise en place du fetch des data et variables d'env

--- src/pages/AllProducts.jsx
+++ src/pages/AllProducts.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import { useEffect } from 'react'
 
 const AllProducts = () => {
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_BACKEND_URL}/actors`)
+      .then(res => res.json())
+      .then(actors => console.log(actors))
+      .catch(err => console.error(err))
+  }, [])
+
   return (
     <div>
       <h1>Mon application de recherche Black && Mortimer</h1>
