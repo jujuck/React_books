e624b8ede90a7aa16b6733d52e9ce56fce896e5c
Julien RICHARD <julien.y.richard@gmail.com>
2023-05-02 12:03:44 +0200

    Récupération de la data d un acteur via son id

--- src/pages/OneProduct.jsx
+++ src/pages/OneProduct.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 const OneProduct = () => {
   const { id } = useParams()
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_BACKEND_URL}/actors/${id}`)
+      .then(res => res.json())
+      .then(actor => console.log(actor))
+      .catch(err => console.error(err))
+  }, [])
 
   return (
     <div>OneProduct</div>
