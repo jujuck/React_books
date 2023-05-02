a916ebc826663b49eff93553918c84116ded2dbb
Julien RICHARD <julien.y.richard@gmail.com>
2023-05-02 12:05:15 +0200

    Mémorisation de la data d'un acteur

--- src/pages/OneProduct.jsx
+++ src/pages/OneProduct.jsx
@@ -1,18 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const OneProduct = () => {
-  const { id } = useParams()
+  const { id } = useParams();
+  const [ actor, setActor] = useState()
 
   useEffect(() => {
     fetch(`${import.meta.env.VITE_BACKEND_URL}/actors/${id}`)
       .then(res => res.json())
-      .then(actor => console.log(actor))
+      .then(actor => setActor(actor))
       .catch(err => console.error(err))
   }, [])
 
   return (
-    <div>OneProduct</div>
+    <div>
+      OneProduct
+    </div>
   )
 }
 
