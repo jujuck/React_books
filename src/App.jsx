a353f7e91bc38bb0ce6b1b97b210359611958ae2
Julien RICHARD <julien.y.richard@gmail.com>
2023-05-02 10:43:16 +0200

    Création dela page AllProducts

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import './App.css'
+import './App.css';
+import AllProducts from './pages/AllProducts';
 
 function App() {
 
   return (
     <>
-      <h1>MOn application de recherche Black && Mortimer</h1>
+      <AllProducts />
     </>
   )
 }
---
+++ src/pages/AllProducts.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AllProducts = () => {
+  return (
+    <div>
+      <h1>Mon application de recherche Black && Mortimer</h1>
+    </div>
+  )
+}
+
+export default AllProducts
